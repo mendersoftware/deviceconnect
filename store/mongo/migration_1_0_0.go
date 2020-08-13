8f15d1c18a6738c33a974325dc1f0b3e29d1607a
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2020-08-13 17:17:37 +0200

    Updated migration 1.0.0: index the devices collection
    Changelog: none
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- store/mongo/migration_1_0_0.go
+++ store/mongo/migration_1_0_0.go
@@ -5,8 +5,12 @@
 package mongo
 
 import (
+	"context"
+
 	"github.com/mendersoftware/go-lib-micro/mongo/migrate"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	mopts "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type migration1_0_0 struct {
@@ -16,6 +20,38 @@ type migration1_0_0 struct {
 
 // Up creates the jobs capped collection
 func (m *migration1_0_0) Up(from migrate.Version) error {
+	ctx := context.Background()
+	database := m.client.Database(m.db)
+
+	collDevices := database.Collection(DevicesCollectionName)
+	idxDevices := collDevices.Indexes()
+
+	// unique index devices.device_id
+	indexOptions := mopts.Index()
+	indexOptions.SetBackground(false)
+	indexOptions.SetName("device_id")
+	indexOptions.SetUnique(true)
+	deviceIDIndex := mongo.IndexModel{
+		Keys:    bson.D{{Key: "device_id", Value: 1}},
+		Options: indexOptions,
+	}
+	if _, err := idxDevices.CreateOne(ctx, deviceIDIndex); err != nil {
+		return err
+	}
+
+	// unique index devices.websocket_id
+	indexOptions = mopts.Index()
+	indexOptions.SetBackground(false)
+	indexOptions.SetName("websocket_id")
+	indexOptions.SetUnique(true)
+	deviceIDIndex = mongo.IndexModel{
+		Keys:    bson.D{{Key: "websocket_id", Value: 1}},
+		Options: indexOptions,
+	}
+	if _, err := idxDevices.CreateOne(ctx, deviceIDIndex); err != nil {
+		return err
+	}
+
 	return nil
 }
 
