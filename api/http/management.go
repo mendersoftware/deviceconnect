merlin-northern/deviceconnect-1

MEN-4316 MEN-4317 Session logging: record and playback.
ChangeLog:none
Signed-off-by: Peter Grzybowski <peter@northern.tech>

api/http/management.go
@@ -15,7 +15,10 @@
 package http
 
 import (
+	"bufio"
 	"context"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
@@ -42,6 +45,18 @@ var (
 	ErrMissingUserAuthentication = errors.New(
 		"missing or non-user identity in the authorization headers",
 	)
+
+	//The name of the field holding a number of milliseconds to sleep between
+	//the consecutive writes of session recording data. Note that it does not have
+	//anything to do with the sleep between the keystrokes send, lines printed,
+	//or screen blinks, we are only aware of the stream of bytes.
+	PlaybackSleepIntervalMsField = "sleep_ms"
+
+	//The name of the field in the query parameter to GET that holds the id of a session
+	PlaybackSessionIDField = "sessionId"
+
+	WebsocketReadBufferSize  = 1024
+	WebsocketWriteBufferSize = 1024
 )
 
 const channelSize = 25 // TODO make configurable
@@ -181,8 +196,8 @@ func (h ManagementController) Connect(c *gin.Context) {
 
 	// upgrade get request to websocket protocol
 	upgrader := websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
+		ReadBufferSize:  WebsocketReadBufferSize,
+		WriteBufferSize: WebsocketWriteBufferSize,
 		Subprotocols:    []string{"protomsg/msgpack"},
 		CheckOrigin: func(r *http.Request) bool {
 			return true
@@ -207,6 +222,71 @@ func (h ManagementController) Connect(c *gin.Context) {
 	h.ConnectServeWS(ctx, conn, session, deviceChan)
 }
 
+func (h ManagementController) Playback(c *gin.Context) {
+	ctx := c.Request.Context()
+	l := log.FromContext(ctx)
+
+	idata := identity.FromContext(ctx)
+	if !idata.IsUser {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": ErrMissingUserAuthentication.Error(),
+		})
+		return
+	}
+
+	tenantID := idata.Tenant
+	userID := idata.Subject
+	sessionID := c.Param(PlaybackSessionIDField)
+	session := &model.Session{
+		TenantID: tenantID,
+		UserID:   userID,
+		StartTS:  time.Now(),
+	}
+	sleepInterval := c.Param(PlaybackSleepIntervalMsField)
+	sleepMilliseconds := uint(app.DefaultPlaybackSleepIntervalMs)
+	if len(sleepInterval) > 1 {
+		n, err := strconv.ParseUint(sleepInterval, 10, 32)
+		if err != nil {
+			sleepMilliseconds = uint(n)
+		}
+	}
+
+	l.Infof("Playing back the session %s", sessionID)
+
+	// upgrade get request to websocket protocol
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  WebsocketReadBufferSize,
+		WriteBufferSize: WebsocketWriteBufferSize,
+		Subprotocols:    []string{"protomsg/msgpack"},
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+		Error: func(
+			w http.ResponseWriter, r *http.Request, s int, e error) {
+			rest.RenderError(c, s, e)
+		},
+	}
+
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		err = errors.Wrap(err, "unable to upgrade the request to websocket protocol")
+		l.Error(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal error",
+		})
+		return
+	}
+
+	deviceChan := make(chan *nats.Msg, channelSize)
+	errChan := make(chan error, 1)
+
+	go h.websocketWriter(ctx, conn, session, deviceChan, errChan, ioutil.Discard)
+
+	h.app.GetSessionRecording(ctx, sessionID, app.NewPlayback(sessionID, deviceChan, sleepMilliseconds))
+	//after this point the UI has to keep the terminal window open by itself to let user
+	//see the playback -- the websocket connection to the deviceconnect is going to be closed
+}
+
 func websocketPing(conn *websocket.Conn) bool {
 	pongWaitString := strconv.Itoa(int(pongWait.Seconds()))
 	if err := conn.WriteControl(
@@ -229,6 +309,7 @@ func (h ManagementController) websocketWriter(
 	session *model.Session,
 	deviceChan <-chan *nats.Msg,
 	errChan <-chan error,
+	recorder io.Writer,
 ) (err error) {
 	l := log.FromContext(ctx)
 	defer func() {
@@ -263,10 +344,36 @@ func (h ManagementController) websocketWriter(
 			time.Now().Add(writeWait),
 		)
 	})
+
+	recorderBuffered := bufio.NewWriterSize(recorder, app.RecorderBufferSize)
+	defer recorderBuffered.Flush()
+	recordedBytes := 0
 Loop:
 	for {
 		select {
 		case msg := <-deviceChan:
+			mr := &ws.ProtoMsg{}
+			err = msgpack.Unmarshal(msg.Data, mr)
+			if err != nil {
+				return err
+			}
+			if mr.Header.Proto == ws.ProtoTypeShell {
+				if mr.Header.MsgType == shell.MessageTypeShellCommand {
+					b, e := recorderBuffered.Write(mr.Body)
+					if e != nil {
+						l.Errorf("session logging: recorderBuffered.Write(len=%d)=%d,%+v", len(mr.Body), b, e)
+					}
+					recordedBytes += len(mr.Body)
+					if recordedBytes >= app.MessageSizeLimit {
+						l.Infof("closing session on limit reached.") //see https://tracker.mender.io/browse/MEN-4448
+						break Loop
+					}
+				} else if mr.Header.MsgType == shell.MessageTypeStopShell {
+					l.Debugf("session logging: recorderBuffered.Flush() at %d on stop shell", recordedBytes)
+					recorderBuffered.Flush()
+				}
+			}
+
 			err = conn.WriteMessage(websocket.BinaryMessage, msg.Data)
 			if err != nil {
 				l.Error(err)
@@ -336,7 +443,7 @@ func (h ManagementController) ConnectServeWS(
 	}()
 	// websocketWriter is responsible for closing the websocket
 	//nolint:errcheck
-	go h.websocketWriter(ctx, conn, sess, deviceChan, errChan)
+	go h.websocketWriter(ctx, conn, sess, deviceChan, errChan, h.app.GetRecorder(ctx, sess.ID))
 
 	var data []byte
 	for {

app/app.go
@@ -16,6 +16,7 @@ package app
 
 import (
 	"context"
+	"io"
 	"time"
 
 	"github.com/google/uuid"
@@ -46,6 +47,9 @@ type App interface {
 	PrepareUserSession(ctx context.Context, sess *model.Session) error
 	FreeUserSession(ctx context.Context, sessionID string) error
 	RemoteTerminalAllowed(ctx context.Context, tenantID, deviceID string, groups []string) (bool, error)
+	GetSessionRecording(ctx context.Context, id string, w io.Writer) (err error)
+	SaveSessionRecording(ctx context.Context, id string, sessionBytes []byte) error
+	GetRecorder(ctx context.Context, sessionID string) *Recorder
 }
 
 // app is an app object
@@ -248,4 +252,18 @@ func (a *app) RemoteTerminalAllowed(
 		return false, nil
 	}
 	return true, nil
+}
+
+func (a *app) GetSessionRecording(ctx context.Context, id string, w io.Writer) (err error) {
+	err = a.store.GetSessionRecording(ctx, id, w)
+	return err
+}
+
+func (a *app) SaveSessionRecording(ctx context.Context, id string, sessionBytes []byte) error {
+	err := a.store.SetSessionRecording(ctx, id, sessionBytes)
+	return err
+}
+
+func (a app) GetRecorder(ctx context.Context, sessionID string) *Recorder {
+	return NewRecorder(ctx, sessionID, a.store)
 }

model/recording.go
@@ -1,0 +1,28 @@
+// Copyright 2021 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+package model
+
+import (
+	"github.com/mendersoftware/go-lib-micro/mongo/oid"
+	"time"
+)
+
+type Recording struct {
+	ID        oid.ObjectID `json:"-" bson:"_id"`
+	SessionID string       `json:"session_id" bson:"session_id"`
+	Recording []byte       `json:"recording" bson:"recording"`
+	CreatedTs time.Time    `json:"created_ts" bson:"created_ts"`
+	ExpireTs  time.Time    `json:"expire_ts" bson:"expire_ts"`
+}

mendersoftware/deviceconnect

api/http/management.go
@@ -15,7 +15,10 @@
 package http
 
 import (
+	"bufio"
 	"context"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
@@ -44,6 +47,18 @@ var (
 	ErrMissingUserAuthentication = errors.New(
 		"missing or non-user identity in the authorization headers",
 	)
+
+	//The name of the field holding a number of milliseconds to sleep between
+	//the consecutive writes of session recording data. Note that it does not have
+	//anything to do with the sleep between the keystrokes send, lines printed,
+	//or screen blinks, we are only aware of the stream of bytes.
+	PlaybackSleepIntervalMsField = "sleep_ms"
+
+	//The name of the field in the query parameter to GET that holds the id of a session
+	PlaybackSessionIDField = "sessionId"
+
+	WebsocketReadBufferSize  = 1024
+	WebsocketWriteBufferSize = 1024
 )
 
 const channelSize = 25 // TODO make configurable
@@ -183,8 +198,8 @@ func (h ManagementController) Connect(c *gin.Context) {
 
 	// upgrade get request to websocket protocol
 	upgrader := websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
+		ReadBufferSize:  WebsocketReadBufferSize,
+		WriteBufferSize: WebsocketWriteBufferSize,
 		Subprotocols:    []string{"protomsg/msgpack"},
 		CheckOrigin: func(r *http.Request) bool {
 			return true
@@ -209,6 +224,82 @@ func (h ManagementController) Connect(c *gin.Context) {
 	h.ConnectServeWS(ctx, conn, session, deviceChan)
 }
 
+func (h ManagementController) Playback(c *gin.Context) {
+	ctx := c.Request.Context()
+	l := log.FromContext(ctx)
+
+	idata := identity.FromContext(ctx)
+	if !idata.IsUser {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": ErrMissingUserAuthentication.Error(),
+		})
+		return
+	}
+
+	tenantID := idata.Tenant
+	userID := idata.Subject
+	sessionID := c.Param(PlaybackSessionIDField)
+	session := &model.Session{
+		TenantID: tenantID,
+		UserID:   userID,
+		StartTS:  time.Now(),
+	}
+	sleepInterval := c.Param(PlaybackSleepIntervalMsField)
+	sleepMilliseconds := uint(app.DefaultPlaybackSleepIntervalMs)
+	if len(sleepInterval) > 1 {
+		n, err := strconv.ParseUint(sleepInterval, 10, 32)
+		if err != nil {
+			sleepMilliseconds = uint(n)
+		}
+	}
+
+	l.Infof("Playing back the session %s", sessionID)
+
+	// upgrade get request to websocket protocol
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  WebsocketReadBufferSize,
+		WriteBufferSize: WebsocketWriteBufferSize,
+		Subprotocols:    []string{"protomsg/msgpack"},
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+		Error: func(
+			w http.ResponseWriter, r *http.Request, s int, e error) {
+			rest.RenderError(c, s, e)
+		},
+	}
+
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		err = errors.Wrap(err, "unable to upgrade the request to websocket protocol")
+		l.Error(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal error",
+		})
+		return
+	}
+
+	deviceChan := make(chan *natsio.Msg, channelSize)
+	errChan := make(chan error, 1)
+
+	//nolint:errcheck
+	go h.websocketWriter(ctx, conn, session, deviceChan, errChan, ioutil.Discard)
+
+	err = h.app.GetSessionRecording(ctx,
+		sessionID,
+		app.NewPlayback(sessionID, deviceChan, sleepMilliseconds))
+	if err != nil {
+		err = errors.Wrap(err, "unable to get the session.")
+		l.Error(err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal error",
+		})
+		return
+	}
+	//after this point the UI has to keep the terminal window open by itself to let user
+	//see the playback -- the websocket connection to the deviceconnect is going to be closed
+}
+
 func websocketPing(conn *websocket.Conn) bool {
 	pongWaitString := strconv.Itoa(int(pongWait.Seconds()))
 	if err := conn.WriteControl(
@@ -231,6 +322,7 @@ func (h ManagementController) websocketWriter(
 	session *model.Session,
 	deviceChan <-chan *natsio.Msg,
 	errChan <-chan error,
+	recorder io.Writer,
 ) (err error) {
 	l := log.FromContext(ctx)
 	defer func() {
@@ -265,10 +357,40 @@ func (h ManagementController) websocketWriter(
 			time.Now().Add(writeWait),
 		)
 	})
+
+	recorderBuffered := bufio.NewWriterSize(recorder, app.RecorderBufferSize)
+	defer recorderBuffered.Flush()
+	recordedBytes := 0
 Loop:
 	for {
 		select {
 		case msg := <-deviceChan:
+			mr := &ws.ProtoMsg{}
+			err = msgpack.Unmarshal(msg.Data, mr)
+			if err != nil {
+				return err
+			}
+			if mr.Header.Proto == ws.ProtoTypeShell {
+				if mr.Header.MsgType == shell.MessageTypeShellCommand {
+					b, e := recorderBuffered.Write(mr.Body)
+					if e != nil {
+						l.Errorf("session logging: "+
+							"recorderBuffered.Write(len=%d)=%d,%+v",
+							len(mr.Body), b, e)
+					}
+					recordedBytes += len(mr.Body)
+					if recordedBytes >= app.MessageSizeLimit {
+						l.Infof("closing session on limit reached.")
+						//see https://tracker.mender.io/browse/MEN-4448
+						break Loop
+					}
+				} else if mr.Header.MsgType == shell.MessageTypeStopShell {
+					l.Debugf("session logging: recorderBuffered.Flush()"+
+						" at %d on stop shell", recordedBytes)
+					recorderBuffered.Flush()
+				}
+			}
+
 			err = conn.WriteMessage(websocket.BinaryMessage, msg.Data)
 			if err != nil {
 				l.Error(err)
@@ -340,7 +462,7 @@ func (h ManagementController) ConnectServeWS(
 
 	// websocketWriter is responsible for closing the websocket
 	//nolint:errcheck
-	go h.websocketWriter(ctx, conn, sess, deviceChan, errChan)
+	go h.websocketWriter(ctx, conn, sess, deviceChan, errChan, h.app.GetRecorder(ctx, sess.ID))
 
 	var data []byte
 	for {

app/app.go
@@ -16,6 +16,7 @@ package app
 
 import (
 	"context"
+	"io"
 	"time"
 
 	"github.com/google/uuid"
@@ -46,6 +47,9 @@ type App interface {
 	PrepareUserSession(ctx context.Context, sess *model.Session) error
 	FreeUserSession(ctx context.Context, sessionID string) error
 	RemoteTerminalAllowed(ctx context.Context, tenantID, deviceID string, groups []string) (bool, error)
+	GetSessionRecording(ctx context.Context, id string, w io.Writer) (err error)
+	SaveSessionRecording(ctx context.Context, id string, sessionBytes []byte) error
+	GetRecorder(ctx context.Context, sessionID string) *Recorder
 }
 
 // app is an app object
@@ -248,4 +252,18 @@ func (a *app) RemoteTerminalAllowed(
 		return false, nil
 	}
 	return true, nil
+}
+
+func (a *app) GetSessionRecording(ctx context.Context, id string, w io.Writer) (err error) {
+	err = a.store.GetSessionRecording(ctx, id, w)
+	return err
+}
+
+func (a *app) SaveSessionRecording(ctx context.Context, id string, sessionBytes []byte) error {
+	err := a.store.InsertSessionRecording(ctx, id, sessionBytes)
+	return err
+}
+
+func (a app) GetRecorder(ctx context.Context, sessionID string) *Recorder {
+	return NewRecorder(ctx, sessionID, a.store)
 }

model/recording.go
@@ -1,0 +1,29 @@
+// Copyright 2021 Northern.tech AS
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Recording struct {
+	ID        uuid.UUID `json:"-" bson:"_id"`
+	SessionID string    `json:"session_id" bson:"session_id"`
+	Recording []byte    `json:"recording" bson:"recording"`
+	CreatedTs time.Time `json:"created_ts" bson:"created_ts"`
+	ExpireTs  time.Time `json:"expire_ts" bson:"expire_ts"`
+}